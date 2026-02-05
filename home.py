425a095a6f0e1a6a6500c3d84704a83f6ccb0fc2
ADMA JINGHAO CHEN <ADAM_CJH@OUTLOOK.COM>
2026-02-05 00:05:45 -0800

    Implement database connection handling and update requirements for SQLAlchemy support

--- home.py
+++ home.py
@@ -21,3 +21,52 @@ pages = {
 
 pg = st.navigation(pages, position='top', expanded=True)
 pg.run()
+
+
+from google.cloud.sql.connector import Connector, IPTypes
+import sqlalchemy
+from google.oauth2 import service_account
+import os
+import json
+
+def get_engine():
+    if "service_account" in st.secrets:
+        sa_info = st.secrets["service_account"]
+    else:
+        sa_info = json.loads(os.environ.get("gcp_service_acc"))
+
+    creds = service_account.Credentials.from_service_account_info(sa_info)
+    connector = Connector(credentials=creds)
+
+    def getconn():
+        if "db_credentials" in st.secrets:
+            conn = connector.connect(
+                st.secrets["db_credentials"]["instance_connection_name"],
+                "pg8000",
+                user=st.secrets["db_credentials"]["db_user"],
+                password=st.secrets["db_credentials"]["db_pass"],
+                db=st.secrets["db_credentials"]["db_name"],
+                ip_type=IPTypes.PUBLIC 
+            )
+        else:
+            conn = connector.connect(
+                os.environ.get("instance_connection_name"),
+                "pg8000",
+                user=os.environ.get("db_user"),
+                password=os.environ.get("db_pass"),
+                db=os.environ.get("db_name"),
+                ip_type=IPTypes.PUBLIC 
+            )
+        return conn
+
+    engine = sqlalchemy.create_engine("postgresql+pg8000://", creator=getconn)
+    return engine
+
+st.title("PostgreSQL + VPC Data")
+
+engine = get_engine()
+
+query = "SELECT * FROM mart_price_news__analysis LIMIT 10"
+with engine.connect() as conn:
+    rows = conn.execute(sqlalchemy.text(query)).fetchall()
+    st.table(rows)
